6f5f34e2ab5e49b0ef3fed7e1a9083602043b40a
Thomas Scarsbrook <scarzybrook@googlemail.com>
2011-10-27 18:47:34 +0100

    Move the drawing of the scoreboard inside a function

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -5,38 +5,41 @@ Raphael.fn.teamBox = function (x,y)
 	return this.rect(x,y, 25, 10 ).attr("fill","#00f").attr("stroke","#00f");
 }
 
-var f1a = paper.teamBox(10,10);
-var f1b = paper.teamBox(35,10);
-var f1c = paper.teamBox(10,20);
-var f1d = paper.teamBox(35,20);
-
-var f2a = paper.teamBox(90,10);
-var f2b = paper.teamBox(115,10);
-var f2c = paper.teamBox(90,20);
-var f2d = paper.teamBox(115,20);
-
-var f3a = paper.teamBox(170,10);
-var f3b = paper.teamBox(195,10);
-var f3c = paper.teamBox(170,20);
-var f3d = paper.teamBox(195,20);
-
-var f4a = paper.teamBox(250,10);
-var f4b = paper.teamBox(275,10);
-var f4c = paper.teamBox(250,20);
-var f4d = paper.teamBox(275,20);
-
-var s1 = paper.rect(50,60,50,20).attr("fill","#00f").attr("stroke","#00f");
-var s2 = paper.rect(210,60,50,20).attr("fill","#00f").attr("stroke","#00f");
-
-var fi = paper.rect(130,110,50,20).attr("fill","#00f").attr("stroke","#00f");
-
-var l11 = paper.path("M35,30L75,60");
-var l21 = paper.path("M115,30L75,60");
-var l32 = paper.path("M195,30L235,60");
-var l42 = paper.path("M275,30L235,60");
-
-var l1i = paper.path("M75,80L155,110");
-var l2i = paper.path("M235,80L155,110");
+function drawScore()
+{
+	var f1a = paper.teamBox(10,10);
+	var f1b = paper.teamBox(35,10);
+	var f1c = paper.teamBox(10,20);
+	var f1d = paper.teamBox(35,20);
+	
+	var f2a = paper.teamBox(90,10);
+	var f2b = paper.teamBox(115,10);
+	var f2c = paper.teamBox(90,20);
+	var f2d = paper.teamBox(115,20);
+	
+	var f3a = paper.teamBox(170,10);
+	var f3b = paper.teamBox(195,10);
+	var f3c = paper.teamBox(170,20);
+	var f3d = paper.teamBox(195,20);
+	
+	var f4a = paper.teamBox(250,10);
+	var f4b = paper.teamBox(275,10);
+	var f4c = paper.teamBox(250,20);
+	var f4d = paper.teamBox(275,20);
+	
+	var s1 = paper.rect(50,60,50,20).attr("fill","#00f").attr("stroke","#00f");
+	var s2 = paper.rect(210,60,50,20).attr("fill","#00f").attr("stroke","#00f");
+	
+	var fi = paper.rect(130,110,50,20).attr("fill","#00f").attr("stroke","#00f");
+	
+	var l11 = paper.path("M35,30L75,60");
+	var l21 = paper.path("M115,30L75,60");
+	var l32 = paper.path("M195,30L235,60");
+	var l42 = paper.path("M275,30L235,60");
+	
+	var l1i = paper.path("M75,80L155,110");
+	var l2i = paper.path("M235,80L155,110");
+}
 
 f1a.click(function () {
 	var animf1a = [{x: 10, y: 10, width: 25, height: 10, transform: "r0", fill: "#00f"},{x: 0, y: 0, width: 250, height: 100, transform: "r360", fill: "#fff"}];
@@ -52,6 +55,5 @@ f1a.click(function () {
 	f1c.stop().animate(animf1c[view], 1000);
 	f1d.stop().animate(animf1d[view], 1000);
 	view = 1 - view;
-});
-
+});	
 
