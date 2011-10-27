e95a954ee9eb62a204a8e52f44ddbd1d240e3ce7
Thomas Scarsbrook <scarzybrook@googlemail.com>
2011-10-27 17:51:24 +0100

    Add a function to draw a no-entry sign

---
+++ js/no_entry.js
@@ -0,0 +1,9 @@
+
+Raphael.fn.noEntry = function (x,y)
+{
+	//X and Y are the co-ordinates of the center of the diagram
+	var set = this.set();
+	set.push(this.circle(x,y,300).attr({fill: "#f00", stroke: "none"}),this.circle(x,y,200).attr({fill: "#fff", stroke: "none"}),this.rect(x-(400/2),y-(100/2),400,100).attr({fill: "#f00", stroke: "none"}).transform("r45 " + x + "," + y));
+}
+
+paper.noEntry(700,300);
