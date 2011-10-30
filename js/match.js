5a14d561da98eb3916e1673d4926f74899364287
Thomas Scarsbrook <scarzybrook@googlemail.com>
2011-10-30 21:25:03 +0000

    Add fade in/out to match object

--- js/match.js
+++ js/match.js
@@ -6,6 +6,14 @@ Raphael.fn.match = function()
 	var line = this.path("M" + (pageWidth/50) + "," + (pageHeight/5) + " l" + (pageWidth*48/50) + ",0").attr("stroke-width","10").attr("stroke-linecap","round").attr("stroke","#00f");
 	var time = this.matchTime();
 	var team = this.matchTeam();
+	set.hide = function()
+	{
+		this.animate({opacity: 0},1000);
+	}
+	set.show = function()
+	{
+		this.animate({opacity: 1},1000);
+	}
 	return this.set().push(line,time,team);
 }
 
