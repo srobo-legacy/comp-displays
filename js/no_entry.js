b379cdd7ef0da6551ed2ccd061522510b267789b
Thomas Scarsbrook <scarzybrook@gmail.com>
2012-03-13 22:57:17 +0000

    Started basing the No-Entry sign on ratios
    Rather than fixed values scaled as a whole.

--- js/no_entry.js
+++ js/no_entry.js
@@ -1,8 +1,12 @@
 
 Raphael.fn.noEntry = function (x,y,r)
 {
+	var rscaler = 1/3;
+	var scaler1 = 0.55228474;
+	var scaler2 = 0.3681898444;
 	//X and Y are the co-ordinates of the center of the diagram
-	var noent = this.path("M 450 0 c -248.52813 0 -450 201.47187 -450 450 c 0 248.52812 201.47187 450 450 450 c 248.52814 0 450 -201.47188 450 -450 c 0 -248.52814 -201.47186 -450 -450 -450 z m 0 150 c 165.68543 0 300 134.31457 300 300 c 0 55.63508 -15.17127 107.71181 -41.5625 152.375 l -410.8125 -410.8125 c 44.66319 -26.39123 96.73992 -41.5625 152.375 -41.5625 z m -258.4375 147.625 l 410.8125 410.8125 c -44.66319 26.39123 -96.73992 41.5625 -152.375 41.5625 c -165.68542 0 -300 -134.31457 -300 -300 c 0 -55.63508 15.17127 -107.71181 41.5625 -152.375 z");
+	var noent = this.path("M " + x + " " + (y-r) + " c " + (-scaler1*r) +" 0 " + (-r) + " " + (1-scaler1)*r + " " + (-r) + " " + r + " c 0 " + (scaler1*r) + " " + (1-scaler1)*r + " " + r + " " + r + " " + r + " c " + (scaler1*r) + " 0 " + r + " " + (scaler1-1)*r + " " + r + " " + (-r) + " c 0 " + (-scaler1*r) + " " + (scaler1-1)*r + " " + (-r) + " " + (-r) + " " + (-r) + " z m 0 " + (rscaler*r) + " c " + (scaler2*r) + " 0 " + (2*rscaler*r) + " 134.31457 300 300 c 0 55.63508 -15.17127 107.71181 -41.5625 152.375 l -410.8125 -410.8125 c 44.66319 -26.39123 96.73992 -41.5625 152.375 -41.5625 z m -258.4375 147.625 l 410.8125 410.8125 c -44.66319 26.39123 -96.73992 41.5625 -152.375 41.5625 c -165.68542 0 -300 -134.31457 -300 -300 c 0 -55.63508 15.17127 -107.71181 41.5625 -152.375 z");
+//	noent.scale(r/450,r/450,x,y);
 	noent.hide = function()
 	{
 		this.animate({opacity: 0},1000);
@@ -14,5 +18,6 @@ Raphael.fn.noEntry = function (x,y,r)
 	return noent;
 }
 
-globNoEntry = paper.noEntry(pageWidth/2,pageHeight/2,pageHeight*9/20);
+globNoEntry = paper.noEntry(pageWidth/2,pageHeight/2,450);
+//globNoEntry = paper.noEntry(pageWidth/2,pageHeight/2,pageHeight*9/20);
 globNoEntry.hide();
